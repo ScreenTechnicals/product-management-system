cb7c228a87ab71365554c1d4c7e52b2be9f6f5cf
Chinmay Sa <68277286+ScreenTechnicals@users.noreply.github.com>
2024-04-29 00:57:50 +0530

    feat: add welcome text to the dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+import { Card, CardBody } from "@nextui-org/react";
+
 export default function Home() {
-  return <main className="w-full md:px-10 p-5"></main>;
+  return (
+    <main className="w-full md:px-10 p-5">
+      <div className="flex place-items-center place-content-center w-full md:h-[60svh]">
+        <Card className="w-full mx-auto md:w-[500px]">
+          <CardBody className="overflow-visible py-2">
+            <h1 className="text-center font-bold md:text-xl text-base">
+              KIPL Store Management System
+            </h1>
+          </CardBody>
+        </Card>
+      </div>
+    </main>
+  );
 }
